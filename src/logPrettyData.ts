163cb41679402447cde2ead83c941f4ead8df63c
Robert Herber <robert@kingstinct.com>
2022-08-28 11:23:42 +0200

    Safe up logPrettyData

--- src/logPrettyData.ts
+++ src/logPrettyData.ts
@@ -1,6 +1,12 @@
-function logPrettyData<T = unknown>(data: T, title?: string): T {
+import stringify from 'fast-safe-stringify'
+
+export function prettyData<T = unknown>(data: T, title?: string): string {
   const titleStr = title ? `${title}: ` : ''
-  console.log(`${titleStr}${JSON.stringify(data, null, 2)}`)
+  return `${titleStr}${stringify(data, undefined, 2)}`
+}
+
+function logPrettyData<T = unknown>(data: T, title?: string): T {
+  console.log(prettyData(data, title))
   return data
 }
 
